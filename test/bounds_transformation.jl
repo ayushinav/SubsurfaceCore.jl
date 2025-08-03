278047963f56214c9978f4253d86b4212c090b48
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2025-08-03 02:58:04 -0400

    chore : formatter

--- test/bounds_transformation.jl
+++ test/bounds_transformation.jl
@@ -1,8 +1,8 @@
 @testitem "inverse tests" tags = [:transformation] begin
-    
+
     x = randn(20)
     m = sigmoid_tf.tf.(100 .* x)
-    @test all(-3 .< m.< 6)
+    @test all(-3 .< m .< 6)
     @test all(sigmoid_tf.itf.(m) .≈ 100 .* x)
 
     m = pow_tf.tf.(x)
