fd18210c915ecd8eee9773bba504dd9afbe1a864
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2025-12-09 16:39:22 -0500

    formatter

--- test/mcmc_test.jl
+++ test/mcmc_test.jl
@@ -4,12 +4,12 @@
 
     include("test_model.jl")
 
-    m = testModel([2.], [4.])
+    m = testModel([2.0], [4.0])
     resp = forward(m, [])
 
     modelD = testModelDistribution(
-        product_distribution([Uniform(0., 3.)]),
-        product_distribution([Uniform(3., 5.)]),
+        product_distribution([Uniform(0.0, 3.0)]),
+        product_distribution([Uniform(3.0, 5.0)]),
     )
 
     respD = testResponseDistribution(normal_dist, normal_dist)
@@ -18,11 +18,11 @@
     n_samples = 10_000
     mcache = mcmc_cache(modelD, respD, n_samples, NUTS())
     chains = stochastic_inverse(resp, err_resp, [], mcache, progress = false);
-    
-    m_list = get_model_list(chains, modelD) 
-    
+
+    m_list = get_model_list(chains, modelD)
+
     @test size(chains.value.data, 1) == n_samples
-    @test all(.≈(mean(chains.value.data, dims = 1)[1,1:2], vcat(m.x1, m.x2), atol = 1e-1))
+    @test all(.≈(mean(chains.value.data, dims = 1)[1, 1:2], vcat(m.x1, m.x2), atol = 1e-1))
 
     @test length(m_list) == n_samples
     @test typeof(first(m_list)) <: testModel
@@ -34,12 +34,12 @@ end
 
     include("test_model.jl")
 
-    m = testModel([2.], [4.])
+    m = testModel([2.0], [4.0])
     resp = forward(m, [])
 
     modelD = testModelDistribution(
-        product_distribution([Uniform(0., 3.)]),
-        product_distribution([Uniform(3., 5.)]),
+        product_distribution([Uniform(0.0, 3.0)]),
+        product_distribution([Uniform(3.0, 5.0)]),
     )
 
     respD = testResponseDistribution(normal_dist, normal_dist)
@@ -49,10 +49,10 @@ end
     mcache = mcmc_cache(modelD, respD, n_samples, SliceSampler())
     chains = stochastic_inverse(resp, err_resp, [], mcache);
 
-    m_list = get_model_list(chains, modelD) 
-    
+    m_list = get_model_list(chains, modelD)
+
     @test size(chains.value.data, 1) == n_samples
-    @test all(.≈(mean(chains.value.data, dims = 1)[1,1:2], vcat(m.x1, m.x2), atol = 1e-1))
+    @test all(.≈(mean(chains.value.data, dims = 1)[1, 1:2], vcat(m.x1, m.x2), atol = 1e-1))
 
     @test length(m_list) == n_samples
     @test typeof(first(m_list)) <: testModel
