618d1098a0af7d849aad4fce83c9225750d7aafc
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2026-04-27 21:48:46 -0400

    fix : more fixes to weakdeps and envs

--- ext/SubsurfaceCorePigeonsExt.jl
+++ ext/SubsurfaceCorePigeonsExt.jl
@@ -1,18 +1,22 @@
 module SubsurfaceCorePigeonsExt
 
+using SubsurfaceCore
+using MCMCChains
+import SubsurfaceCore: stochastic_inverse
 using Pigeons
 
 function stochastic_inverse(
-        r_obs::resp1, err_resp::resp2, vars, alg_cache::mcmc_cache, sampler,
-        n_samples=10, ::Val{Pigeons}; n_chains=1, model_trans_utils::NamedTuple=(;), # need to take care of this
+        ::Val{Pigeons}, r_obs::resp1, err_resp::resp2, vars, alg_cache::mcmc_cache,
+        sampler, n_samples=10; n_chains=1, model_trans_utils::NamedTuple=(;), # need to take care of this
         response_trans_utils::NamedTuple=(;), params=(;),
         kwargs...) where {resp1 <: AbstractResponse, resp2 <: AbstractResponse}
-    mcmc_model,
-    counter_var = get_stochastic_inverse_model(r_obs, err_resp, vars, alg_cache; n_chains,
+    
+    # @assert typeof(sampler).name.module===Pigeons "
+    mcmc_model, counter_var = get_stochastic_inverse_model(
+        r_obs, err_resp, vars, alg_cache; n_chains,
         model_trans_utils, response_trans_utils, params)
 
-    @assert typeof(sampler).name.module === Pigeons ""
-    n_rounds = Int(round(log2(alg_cache.n_samples)))
+    n_rounds = Int(round(log2(n_samples)))
     pt = pigeons(; target=TuringLogPotential(mcmc_model), n_chains=n_chains, # Λ ~ 6
         n_rounds=n_rounds,   # low to speed up CI
         record=[traces; round_trip; record_default()], kwargs...)
--- ext/SubsurfaceCoreTuringExt.jl
+++ ext/SubsurfaceCoreTuringExt.jl
@@ -1,41 +1,36 @@
 module SubsurfaceCoreTuringExt
 
+using SubsurfaceCore
+import SubsurfaceCore: stochastic_inverse
 using Turing
 
+# function stochastic_inverse(
+#         ::Val{Turing.Inference}, r_obs::resp1, err_resp::resp2, vars, alg_cache::mcmc_cache,
+#         sampler, n_samples=10; n_chains=1, model_trans_utils::NamedTuple=(;),
+#         response_trans_utils::NamedTuple=(;), params=(;),
+#         kwargs...) where {resp1 <: AbstractResponse, resp2 <: AbstractResponse}
+    
+#     stochastic_inverse(
+#         Val(Turing), r_obs, err_resp, vars, alg_cache,
+#         sampler, n_samples; n_chains, model_trans_utils,
+#         response_trans_utils, params, kwargs...)
+
+#     return chains
+# end
+
 function stochastic_inverse(
-        r_obs::resp1, err_resp::resp2, vars, alg_cache::mcmc_cache, sampler,
-        n_samples=10, ::Val{Turing}; n_chains=1, model_trans_utils::NamedTuple=(;), # need to take care of this
+        ::Val{Turing}, r_obs::resp1, err_resp::resp2, vars, alg_cache::mcmc_cache,
+        sampler, n_samples=10; n_chains=1, model_trans_utils::NamedTuple=(;),
         response_trans_utils::NamedTuple=(;), params=(;),
         kwargs...) where {resp1 <: AbstractResponse, resp2 <: AbstractResponse}
-    mcmc_model,
-    counter_var = get_stochastic_inverse_model(r_obs, err_resp, vars, alg_cache; n_chains,
+    mcmc_model, counter_var = get_stochastic_inverse_model(
+        r_obs, err_resp, vars, alg_cache; n_chains,
         model_trans_utils, response_trans_utils, params)
 
     chains = Turing.sample(mcmc_model, sampler, n_samples; verbose=false, kwargs...)
 
     @info "$(counter_var.c) forward calls ran"
     return chains
-end
-
-function stochastic_inverse(r_obs::resp1,
-        err_resp::resp2,
-        vars,
-        alg_cache::mcmc_cache,
-        sampler::S,
-        ::Val{Turing};
-        n_chains=1,
-        model_trans_utils::NamedTuple=(;), # need to take care of this
-        response_trans_utils::NamedTuple=(;),
-        params=(;),
-        kwargs...) where {resp1 <: AbstractResponse, resp2 <: AbstractResponse,
-        S <: Turing.AdvancedVI.VariationalInference}
-    mcmc_model,
-    counter_var = get_stochastic_inverse_model(r_obs, err_resp, vars, alg_cache; n_chains,
-        model_trans_utils, response_trans_utils, params)
-
-    vi_model = vi(mcmc_model, alg_cache)
-    @info "$(counter_var.c) forward calls ran"
-    return vi_model
 end
 
 end
