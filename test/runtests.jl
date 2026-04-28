1bd3495b3d1a13601d1ca47dbfd274fb8daf75dd
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2026-04-28 01:19:11 -0400

    fix : test CI fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,26 @@
-using ReTestItems, InteractiveUtils, Hwloc, JET, BenchmarkTools, Test
-
-@info sprint(versioninfo)
+using Pkg
 
 const GROUP = lowercase(get(ENV, "GROUP", "all"))
+
+if GROUP == "turing"
+    Pkg.activate(joinpath(@__DIR__, "turing"))
+    Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
+    Pkg.instantiate()
+elseif GROUP == "pigeons"
+    Pkg.activate(joinpath(@__DIR__, "pigeons"))
+    Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
+    Pkg.instantiate()
+end
+
+using ReTestItems, InteractiveUtils, Hwloc, SubsurfaceCore
+
+@info sprint(versioninfo)
 
 const RETESTITEMS_NWORKERS = parse(
     Int, get(ENV, "RETESTITEMS_NWORKERS", string(min(Hwloc.num_physical_cores(), 4))))
 const RETESTITEMS_NWORKER_THREADS = parse(Int,
     get(ENV, "RETESTITEMS_NWORKER_THREADS",
         string(max(Hwloc.num_virtual_cores() ÷ RETESTITEMS_NWORKERS, 1))))
-
-using SubsurfaceCore
 
 @info "Running tests with $(RETESTITEMS_NWORKERS) workers and \
        $(RETESTITEMS_NWORKER_THREADS) threads for group $(GROUP)"
